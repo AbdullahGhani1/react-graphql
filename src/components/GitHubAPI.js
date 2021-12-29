161e2e0b876ae33257da634162eca0bbe4ce0a05
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2021-12-29 18:05:57 +0500

    github restApi call repos

--- src/components/GitHubAPI.js
+++ src/components/GitHubAPI.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios"; // for API Calling
 
 const api = axios.create({
-  baseUrl: "https://api.github.com",
+  baseURL: "https://api.github.com",
 });
 
 function GitHubAPI() {
@@ -13,11 +13,11 @@ function GitHubAPI() {
   useEffect(() => {
     // Immediately Invoked Function Expression - IIFE
     (async () => {
-      const { data: user } = await api.get("/users/abdullahghani1"); // user Data
+      const { data: user } = await api.get("/users/abdullahGhani1"); // user Data
       console.log("user ", user);
       setUserData(user); // update state of the userData
 
-      const { data: repo } = await api.get("/users/uabdullahghani1qutub/repos");
+      const { data: repo } = await api.get("/users/abdullahGhani1/repos");
       console.log("repos: ", repo);
       setUserRepos(repo); // update state of the userRepos        // user repos
     })();
