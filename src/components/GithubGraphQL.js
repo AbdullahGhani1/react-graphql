0f4c060d8b5733302255b0048c92b42080bb72b7
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2021-12-30 01:51:23 +0500

    GraphQL mutation

--- src/components/GithubGraphQL.js
+++ src/components/GithubGraphQL.js
@@ -41,37 +41,57 @@ function GithubGraphQL() {
   //   };
   //   =================================================================================================
 
-  const feetchData_js = async (organizationName) => {
-    const QUERY_ORGANIZATION = `query queryOrganization($organizationName: String!){
-          organization(login:"$organizationName"){
-              name
-              description
-              url
-              createdAt
-          }
-          viewer{
-              login
-              email
-              company
-              repositories(first:5){
-                  edges{
-                      node{
-                          name
-                      }
-                  }
-              }
-          }
-        }`;
-    const res = await api.post("/graphql", {
-      query: QUERY_ORGANIZATION,
-      variables: { organizationName },
+  //   const feetchData_js = async (organizationName) => {
+  //     const QUERY_ORGANIZATION = `query queryOrganization($organizationName: String!){
+  //           organization(login:"$organizationName"){
+  //               name
+  //               description
+  //               url
+  //               createdAt
+  //           }
+  //           viewer{
+  //               login
+  //               email
+  //               company
+  //               repositories(first:5){
+  //                   edges{
+  //                       node{
+  //                           name
+  //                       }
+  //                   }
+  //               }
+  //           }
+  //         }`;
+  //     const res = await api.post("/graphql", {
+  //       query: QUERY_ORGANIZATION,
+  //       variables: { organizationName },
+  //     });
+  //     console.log("GraphQL Response: ", res);
+  //   };
+
+  // =================Mutation =============
+  const addStar = async (repositoryId) => {
+    const MUTATION_ADD_START = `mutation AddStartToMyRepo($abc: ID!){
+        addStar(input: {starrableId: $abc}){
+            starrable {
+                viewerHasStarred
+            }
+        }
+    }`;
+    const {
+      data: { data: addStar },
+    } = await api.post("/graphql", {
+      query: MUTATION_ADD_START,
+      variables: { abc: repositoryId },
     });
-    console.log("GraphQL Response: ", res);
+    console.log("GraphQL Response: ", addStar);
   };
 
   useEffect(() => {
-    feetchData_js("google");
+    // feetchData_js("google"); //querry
+    addStar("R_kgDOGmNF5w"); // Mutation
   }, []);
+
   return (
     <div>
       <h3>User Data</h3>
